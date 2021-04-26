20dd05bf6deeee3e0aeb264b86e98149f8a14150
isayyad498 <66631640+isayyad498@users.noreply.github.com>
2021-04-27 00:16:05 +0530

    Update hydersa.tf

--- hydersa.tf
+++ hydersa.tf
@@ -1,16 +1,7 @@
 provider "aws" {
 region = "eu-west-2"
-access_key = ""
-secret_key = ""
-}
-resource "aws_elasticache_cluster" "sam_cluster" {
-cluster_id = "sameer"
-engine = "redis"
-node_type = "cache.t2.micro"
-num_cache_nodes = "1"
-parameter_group_name = "default.redis3.2"
-engine_version = "3.2.10"
-port = "6379"
+access_key = "AKIAX4EFVBORR4JO6UHJ"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "mybucket" {
 bucket = "imran6667788ii"
